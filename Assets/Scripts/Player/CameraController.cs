cbb11d9ee69da38503837edf053d42d388063f77
Qvelent <qvelent@gmail.com>
2023-05-13 14:27:16 +0300

    Add all

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+
+    public Transform player;
+
+
+    [Header("чувствительность мыши")]
+    public float sensivityMouse = 200f;
+
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CursorLock();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Look();
+        RangeToUseItems();
+    }
+
+    public void CursorLock()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //заблокировали курсор
+    }
+
+    public void CursorAnLock()
+    {
+        Cursor.lockState = CursorLockMode.None; //разблокировали курсор
+    }
+
+    public GameObject RangeToUseItems()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1.5f))
+        {
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+
+    public void Look()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensivityMouse * Time.deltaTime; // значение инпут.гетахис дает от -1 до 1, а с помощью сенсивити мы умножаем это число и получаем другую скорость поворта. Тайм.делтьа тайм чисто для регулировки кадров и чтобы убрать разрыв кадров(или чет такое)
+        mouseY = Input.GetAxis("Mouse Y") * sensivityMouse * Time.deltaTime;
+
+        xRotation -= mouseY; // убираем инверсию. Называем хротейшен так как чтобы изменить по оси У надо в самом юнити менять по осе Х
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // фиксация значения, то есть передаем переменную и максимальное значение камеры поворота 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //кватрион делает вектор, точнее в нашу переменную он присвоит хротейшен в Х 
+
+
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform characterSize;
+
+
+    [Header("Гравитация")]
+    [SerializeField] private float verticalVelocity;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float jumpForce = 5.0f;
+
+    [Header("Скорость нашего чара")]
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float runSpeed = 3f;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        controller.Move(new Vector3(0f, 1f, 0));
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+       
+        PlayerReset();
+    }
+
+    
+    /// <summary>
+    /// Задает перемещение игрока
+    /// </summary>
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = 1f;
+        }
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(Vector3.ClampMagnitude(move.normalized, 2 * speed * Time.deltaTime));
+    }
+
+
+    /// <summary>
+    /// Респавнит в определенной точке Игрока
+    /// </summary>
+    private void PlayerReset()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            controller.Move(new Vector3(0, 10f, 0));
+        }
+    }
+}
