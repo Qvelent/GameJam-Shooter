2e4ef70715d69c6662ad38a1daa14d0b5410d699
Qvelent <qvelent@gmail.com>
2023-05-14 10:14:39 +0300

    add triggers

---
+++ Assets/Scripts/EnterTrigger.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterTrigger : MonoBehaviour
+{
+    [SerializeField] private string _tag;
+    [SerializeField] private EnterEvent _action;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(_tag));
+
+        _action?.Invoke(other.gameObject);
+    }
+
+    [Serializable]
+    public class EnterEvent : UnityEvent<GameObject>
+    {
+    }
+}
