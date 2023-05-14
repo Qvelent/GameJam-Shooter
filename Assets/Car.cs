aabb235a1d0b81f303b41b9cb3c7abb6d0e583d8
Fantomext <Fantomext80@gmail.com>
2023-05-14 08:10:45 +0300

    add car

---
+++ Assets/Car.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    Rigidbody _rigibody;
+    [SerializeField] private float force = 1f;
+    [SerializeField] private float angleChangeSpeed = 3f;
+
+    void Start()
+    {
+        _rigibody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        _rigibody.AddForce(transform.forward * force);
+        _rigibody.AddTorque(transform.up * angleChangeSpeed);
+    }
+}
